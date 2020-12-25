696ebbf45d6d064ec898e87cd3a2a78ed83c5849
Erkin Verbeek <Erkin.Verbeek@Trincoll.edu>
2020-12-25 16:35:01 +0300

    Add nodal degree color-coding to point graph

--- +SIS_Model/SimAndPlot_BinaryVsODE_PointGraph.m
+++ +SIS_Model/SimAndPlot_BinaryVsODE_PointGraph.m
@@ -6,10 +6,12 @@ function [] = SimAndPlot_BinaryVsODE_PointGraph(Parameters)
 
     %% Setup
 
-    adjacencyMatrix = CreateAdjacencyMatrix(Parameters.N, Parameters.k);
+    N = Parameters.N;
+    
+    adjacencyMatrix = CreateAdjacencyMatrix(N, Parameters.k);
 
     initialNodes = CreateInitialNodes(...
-        Parameters.initialInfectionChance, Parameters.N);
+        Parameters.initialInfectionChance, N);
 
     %% Simulate
 
@@ -19,14 +21,20 @@ function [] = SimAndPlot_BinaryVsODE_PointGraph(Parameters)
         Parameters.gamma, Parameters.length, Parameters.deltaT);
     
     % calculate average time spent infected for each node
-    % over second half of all timesteps
-    ratioSpentInfected_Binary = zeros(1, length(nodes{1}));
-    for node_i = 1:length(nodes{1})
+    % only starting from cutOffTime and onwards
+    nodal_degree = zeros(N, 1);
+    ratioSpentInfected_Binary = zeros(N, 1);
+    for node_i = 1:N
         total = 0;
-        for timestep = 1:length(nodes)
-            total = total + (nodes{timestep}(node_i) == Node.Infected);
+        for timestep = ...
+                Parameters.cutOffTime/Parameters.deltaT : length(nodes)
+            total = total + (nodes{timestep, 1}(1, node_i) == Node.Infected);
         end
-        ratioSpentInfected_Binary(node_i) = total/length(nodes);
+        ratioSpentInfected_Binary(node_i) = total/...
+            (length(nodes) + 1 - Parameters.cutOffTime/Parameters.deltaT);
+        
+        nodal_degree(node_i) = sum(adjacencyMatrix(:, node_i));
+        
     end
     
     
@@ -37,13 +45,46 @@ function [] = SimAndPlot_BinaryVsODE_PointGraph(Parameters)
         Parameters.gamma, Parameters.length, Parameters.deltaT);
     
     % get the final probabilities for each node
-    finalProbabilities_ODE(:) = probabilities_ODE(end,:);
+    finalProbabilities_ODE = zeros(N, 1);
+    for i = 1:N
+        finalProbabilities_ODE(i) = probabilities_ODE(end, i);
+    end
+    %finalProbabilities_ODE(:) = probabilities_ODE(end,:);
     
+    % get the nodal degree for each node
+    %nodal_degree = zeros(N, 1);
+    for i = 1:N
+        
+        % count how many other nodes this node is connected towards 
+        % (j -> i)
+        %nodal_degree(i) = sum(adjacencyMatrix(:, i));
+        
+        
+    end
+    max_degree = max(nodal_degree);
+    
+    color_gradient = cell(max_degree, 1);
+    
+    for i = 1:max_degree + 1
+        color_gradient{i} = [0 + (i-1)/max_degree 1 - (i-1)/max_degree 0];
+    end
     
     %% Plot
     
     % plot combined figure
-    scatter(finalProbabilities_ODE, ratioSpentInfected_Binary);
+    close all
+    hold on
+    for i = 1:N
+        scatter(finalProbabilities_ODE(i), ...
+            ratioSpentInfected_Binary(i), 'MarkerEdgeColor', ...
+            color_gradient{nodal_degree(i)});
+    end
+    hold off
+    
+%    annotation('textbox', [0.15, 0.8, 0.2, 0.1], ...
+%        'String', "Green = smaller nodal degree");
+%    annotation('textbox', [0.15, 0.7, 0.2, 0.1], ...
+%        'String', "Red = higher nodal degree");
     ylim([0,1]);
     xlim([0,1]);
     title("Individual Nodes [Binary vs ODE]");
